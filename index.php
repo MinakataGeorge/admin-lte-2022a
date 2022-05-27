100fea05ebd569ff2b172a85c12e7042bede7cb0
MinakataGeorge <blebee48@gmail.com>
2022-05-26 19:06:26 -0500

    Layouts of footer and header for the index

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include './layouts/header-index.php' ?>
+            <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                <h1 class="m-0">Inicio</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+        <section class="content"> 
+            <div class="container-fluid"> 
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <div class="card-header">
+                              <h3 class="card-title">
+                                <i class="fas fa-chart-pie mr-1"></i>
+                                Administración de tickets
+                              </h3>
+                            </div><!-- /.card-header -->
+
+                            <div class="card-body">
+
+                                <p>Esta es la descripcion de mi sitio</p>
+                                <p>Esta es la descripcion de mi sitio1</p>
+                                <p>Esta es la descripcion de mi sitio2</p>
+                            </div><!-- /.card-body -->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    <?php include './layouts/footer-index.php' ?>
